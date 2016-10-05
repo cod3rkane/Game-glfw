75319e6da32cfdd3cbe6a3d10465504e846dcb32
cod3rkane <jcesar.developer@gmail.com>
2016-10-04 21:11:31 -0300

    Fix Cube 3D

--- components/Cube.cpp
+++ components/Cube.cpp
@@ -209,7 +209,9 @@ void Cube::render() {
 
     glBindVertexArray(this->VAO);
 //    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glEnable(GL_DEPTH_TEST);
     glDrawArrays(GL_TRIANGLES, 0, 36);
+    glDisable(GL_DEPTH_TEST);
     glBindVertexArray(0);
 }
 
