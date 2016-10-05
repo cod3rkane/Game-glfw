b44688b7e80fbf6e50b13ad4aec9c8e800dba141
cod3rkane <jcesar.developer@gmail.com>
2016-10-04 21:11:08 -0300

    Update Camera Sensitivity

--- util/Camera.h
+++ util/Camera.h
@@ -20,7 +20,7 @@ enum Camera_Movement {
 const GLfloat YAW = -90.0f;
 const GLfloat PITCH = 0.0f;
 const GLfloat SPEED = 3.0f;
-const GLfloat SENSITIVITY = 0.25f;
+const GLfloat SENSITIVITY = 0.2f;
 const GLfloat ZOOM = 45.0f;
 
 class Camera {
